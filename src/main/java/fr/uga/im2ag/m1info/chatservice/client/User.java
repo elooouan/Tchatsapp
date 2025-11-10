550b1ed52a5c58095f34b9bd8b91725b247a5685
Loic GIRAUD <lgiraud.giraud@gmail.com>
2025-11-10 15:00:56 +0100

    User.java

---
+++ src/main/java/fr/uga/im2ag/m1info/chatservice/client/User.java
@@ -0,0 +1,24 @@
+package fr.uga.im2ag.m1info.chatservice.client;
+
+public class User {
+    private final int userId;
+    private String pseudo;
+
+    User(int userId, String pseudo) {
+        this.userId = userId;
+        this.pseudo = pseudo;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+}
