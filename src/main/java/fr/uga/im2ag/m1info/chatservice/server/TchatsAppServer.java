efefaa6816b108fc38ae3d7eeef3b1feab34148d
samuel38fr <samuel38fr@hotmail.fr>
2025-11-18 22:38:37 +0100

    getServerState is now public

--- src/main/java/fr/uga/im2ag/m1info/chatservice/server/TchatsAppServer.java
+++ src/main/java/fr/uga/im2ag/m1info/chatservice/server/TchatsAppServer.java
@@ -415,7 +415,7 @@ public class TchatsAppServer implements PacketSender {
 
     }
 
-    private ServerState getServerState(){
+    public ServerState getServerState(){
         return serverState;
     }
 
